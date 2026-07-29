463e9e4be36eeab767e52054e673587c48442b8d
AbduganiyevMuhammadali <muhammadalijunior07@gmail.com>
2026-07-29 15:26:01 +0500

    ozgarish

--- config/config.js
+++ config/config.js
@@ -1,13 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const base = {
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASS,
+  "database": process.env.DB_DATABASE,
+  "port": process.env.DB_PORT,
+  "host": process.env.HOST,
+  "dialect": "mysql",
+};
+
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_DATABASE,
-    "port": process.env.DB_PORT,
-    "host": process.env.HOST,
-    "dialect": "mysql",
-  },
+  "development": base,
+  "test": base,
+  "production": base,
 }
